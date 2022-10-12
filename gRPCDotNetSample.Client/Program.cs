ef28ec075a7c19863563f372c0acad5eb232563f
Will <doggy.huang@gmail.com>
2022-10-12 22:42:41 +0800

    加入 gRPC Client 用戶端程式碼

--- gRPCDotNetSample.Client/Program.cs
+++ gRPCDotNetSample.Client/Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Grpc.Net.Client;
+using gRPCDotNetSample.Service;
+
+using var channel = GrpcChannel.ForAddress("https://localhost:7021");
+var client = new Greeter.GreeterClient(channel);
+var reply = client.SayHello(new HelloRequest { Name = "GreeterClient" });
+
+Console.WriteLine("Greeting: " + reply.Message);
+Console.WriteLine("Press any key to exit...");
+Console.ReadKey();
